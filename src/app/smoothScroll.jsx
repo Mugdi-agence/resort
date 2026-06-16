3625719fe2731e48a443f3d096a12de325674661
Mugdi-agence <contact.mugdi@gmail.com>
2026-06-16 19:55:32 +0200

    resort

--- src/app/smoothScroll.jsx
+++ src/app/smoothScroll.jsx
@@ -5,23 +5,23 @@ import Lenis from "lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
 
 export default function SmoothScroll({ children }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.0,        // plus bas = plus rapide / plus snappy
+      duration: 1.0,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 1.5,
-      autoRaf: false,        // on gère le RAF nous-mêmes via gsap.ticker
+      autoRaf: false,
     });
 
-    // Synchronise Lenis avec ScrollTrigger
     lenis.on("scroll", ScrollTrigger.update);
 
-    // Branche Lenis sur la boucle GSAP (même tick, pas de décalage)
     const update = (time) => lenis.raf(time * 1000);
     gsap.ticker.add(update);
     gsap.ticker.lagSmoothing(0);
